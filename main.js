f3652f34caced8a0bdc1b9225dfec03fd267baad
Shikirra Williams <ShikirraWilliams@shikirras-mbp.lan>
2017-09-08 16:43:25 -0400

    fixed music functionality and fetch call

--- main.js
+++ main.js
@@ -8,75 +8,63 @@
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
 
-let musicPlayer = document.getElementsByClassName('music-player');
-let results;
+let musicPlayer = document.getElementsByClassName('music-player')[0];
+let  searchMusic = document.getElementById('submit');
+let results = document.getElementById('results');
+
+console.log('Results div: ', results);
+
+searchMusic.addEventListener('click', function(a) {
+  let input = document.getElementById('field');
+console.log(input.value);
+
+let searchInfo = input.value.split('').join("+");
 
 
-let artistSearch = document.querySelector('find');
-let submit = document.getElementsByClassName('search-form');
-artistSearch.addEventListener('onclick', buttonAction);
-console.log(artistSearch);
-
-
-function buttonAction() {
-
-fetch("https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/", "POST")
+fetch("https://itunes.apple.com/search?term=" + searchInfo + "&entity=song")
   .then(
     function(response) {
       if (response.status !== 200) {
         console.log(response.status);
         return;
       }
-      response.json().then(function(data) {
 
-        console.log("Here is the data:", data);
+      response.json().then(function(data) {
+        displaySong(data);
+        // console.log("Here is the data:", data.results);
       });
     }
   )
   .catch(function(err) {
     console.log("Fetch Error :-S", err);
   });
+})
+
+
+function displaySong(data) {
+  let template = '';
+
+  for (var i = 0; i < data.results.length; i++) {
+
+    template +=
+    `
+    <div class="songs">
+    <img src="${data.results[i].artworkUrl100}">
+    <h3>${data.results[i].artistName}</h3>
+    <h5>${data.results[i].trackName}</h5>
+    </div>
+    `;
+
+  }
+  results.innerHTML = template;
+  let song = document.getElementsByClassName('songs');
+console.log("songs are: ", song);
+    for (var i = 0; i < song.length; i++) {
+      let result = i
+      song[i].addEventListener('click', function() {
+        console.log("result is: ", data.results[result], result);
+        musicPlayer.src = data.results[result].previewUrl;
+        musicPlayer.play();
+      })
+    }
 }
-
-// function functionName() {
-//
-// }
-
-
-
-
-//  let url= "https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/"
-//   let submit = document.getElementsByTagName('button');
-//   let artistSearch = document.getElementsByClassName('search-form')
-//
-// fetch(url, {
-//     method: 'POST',
-//     body: JSON.stringify() ,
-//     headers: {
-//       "Content-Type": "application/json"
-//     }
-//   })
-//   .then(function(response) {
-//     return response.json ();
-//   }).then(function(data) {
-//     console.log(data);
-//   })
-
-
-// submit.addEventListener('click', function() {
-//   fetchData();
-//
-// })
-//
-// function fetchData() {
-//   fetch  (url)
-//     .then(function(res) {
-//       return res.json();
-//     }).then(function(data) {
-// let load = data.payload;
-// for(var i=0; i < load.length;  i++) {
-//   console.log(load[i].username);
-//   console.log(load[i].password);
-// }
-// })
-// }
